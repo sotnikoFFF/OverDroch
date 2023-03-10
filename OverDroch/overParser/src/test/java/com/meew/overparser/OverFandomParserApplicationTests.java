5deda104328aea4721dc2d330779dffa09667c22
SotnikoFFF <grisha.sotnikov@gmail.com>
2023-03-10 13:52:42 +0200

    add missing files

---
+++ OverDroch/overParser/src/test/java/com/meew/overparser/OverFandomParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meew.overparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverFandomParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
